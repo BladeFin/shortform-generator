36fbc818159b7ed46c86275b1598e27a8993957d
Connor Koefelda <bladefin99@gmail.com>
2024-12-01 01:05:41 -0600

    second versions

--- combine.py
+++ combine.py
@@ -1,45 +1,89 @@
-from moviepy.editor import *
-import ffmpeg
+import subprocess
 #crop video
 #add audio to video
 #add srt subs to video
 
-def createVideo(videoPath, audioPath, srtPath):
-    #load video and audio
-    video = VideoFileClip(videoPath)
-    audio = AudioFileClip(audioPath)
-    print(type(audio))
-
-    video = trimVideoToAudio(video, audio)
-    video = addAudioToVideo(video, audio)
-    video = saveTempVidFile(video)
+def createVideo(video_path, audio_path, srt_path):
     #get audio length
-    """
-    srt = open(srtPath, "r")
+    audio_duration = getAudioDuration(srt_path)
+    trimVideo(video_path, audio_duration)
+    addSubsToVideo("temp_trimmed_video.mp4",srt_path)
+    addAudioToVideo("temp_trimmed_subs_video.mp4", audio_path)
+    
+def getAudioDuration(srt_path):
+    srt = open(srt_path, "r")
     lines = [line for line in srt]
-    final_codes = lines[len(lines)-4]
-    final_code = final_codes[len(final_codes)-12]
-    audio_length = final_code[:2]*360 + final_code[3:5]*60 + final_code[6:8] + final_code[9:]*.001
-    """
+    last_stamps = lines[len(lines)-3] #00:03:24,040 --> 00:03:25,960
+    last_stamp = last_stamps[len(last_stamps) - len("00:00:00,000")-1:] #00:03:25,960
+    print("LAST STAMP: " + last_stamp)
+    duration = f"{last_stamp[:8]}.{last_stamp[9:-2]}" #00:03:25.960
+    print("THE DURATION" + duration)
+    return duration
 
-    pass
+def trimVideo(video_path, audio_duration):
+    try:
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i", video_path,
+            "-ss", "00:00:00.000",
+            "-to", audio_duration,
+            "-c", "copy",
+            "temp_trimmed_video.mp4"
+            ]
+        
+        subprocess.run(command, check=True)
+    
+    except subprocess.CalledProcessError as e:
+        print(f" Something went wrong while cutting the video: {e}")
+    except FileNotFoundError:
+        print("FFmpeg isn't around D:")
 
-#takes videoClip and audioClip, cuts videoClip to be only as long as audio clip
-def trimVideoToAudio(video, audio):
-    print(type(audio))
-    trimmed_video = video.subclip(0, audio.duration)
-    return trimmed_video
+def addSubsToVideo(video_path, srt_path):
+    try:
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i", video_path,
+            "-vf", f"subtitles={srt_path}:force_style='Alignment=6,MarginV=140'",
+            "-c:a", "copy",
+            "temp_trimmed_subs_video.mp4"
 
-#takes videoClip and audioClip, adds audio t ovideo
-def addAudioToVideo(video, audio):
-    video = video.set_audio(audio)
-    return video
+        ]
 
-def saveTempVidFile(video):
-    video.write_videofile("temp_putput.mp4")
+        subprocess.run(command, check=True)
 
-videoPath = "videoplayback.mp4"
-audioPath = "test_audio.mp3"
-srtPath = "sub.srt"
+    except subprocess.CalledProcessError as e:
+        print(f" Something went wrong while cutting the video: {e}")
+    except FileNotFoundError:
+        print("FFmpeg isn't around D:")
 
-createVideo(videoPath, audioPath, srtPath)
+def addAudioToVideo(video_path, audio_path):
+    try:
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i", video_path,
+            "-i", audio_path,
+            "-c", "copy",
+            "-map", "0:v:0",
+            "-map", "1:a:0",
+            "-shortest",
+            "output.mp4"
+        ]
+    
+        subprocess.run(command, check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f" Something went wrong while cutting the video: {e}")
+    except FileNotFoundError:
+        print("FFmpeg isn't around D:")
+
+
+    
+
+video_path = "tinyplayback.mp4"
+audio_path = "test_audio.mp3"
+srt_path = "sub.srt"
+
+createVideo(video_path, audio_path, srt_path)
