5d3220258c85f2651cf3167b916996c8970bd36f
Connor Koefelda <bladefin99@gmail.com>
2024-12-17 22:51:49 -0600

    added checkExistence, flushTempFiles, readFileAsString (takes script as a file now), segmentVideo

--- main.py
+++ main.py
@@ -1,50 +1,47 @@
 from tts import generateTTS
 from srt import mp3_to_srt
 from combine import createVideo
+from segment import segmentVideo
 import os
 
-def generateViralVideo(script, temp_audio_output, temp_srt_output, source_video_path, video_output_path, randomize=False, karaoke=True, lang='en', tld='com', flush=True):
-    #generateTTS(script, temp_audio_output, lang=lang, tld=tld)
-    #mp3_to_srt(temp_audio_output, temp_srt_output, karaoke=karaoke)
+def generateViralVideo(script_path, temp_audio_output, temp_srt_output, source_video_path, video_output_path, randomize=False, karaoke=True, lang='en', tld='com', flush=True):
+    checkExistence([script_path, source_video_path])
+    script = readFileAsString(script_path)
+    generateTTS(script, temp_audio_output, lang=lang, tld=tld)
+    mp3_to_srt(temp_audio_output, temp_srt_output, karaoke=karaoke)
     createVideo(source_video_path, temp_audio_output, temp_srt_output, video_output_path, randomize=randomize)
     if (flush):
+        flushTempFiles([temp_audio_output, temp_srt_output])
+    segmentVideo(video_output_path)
+    
+
+#Checks if all of the files in path_list exist, raising an error if any are missing
+def checkExistence(path_list):
+    for path in path_list:
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Error: The file '{path}' does not exist.  Generation halted.")
+
+#returns the contents of a text file as a strings
+def readFileAsString(text_file_path):
+
+    with open(text_file_path, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+
+    return file_content
+
+#Deletes all files in path_list
+def flushTempFiles(path_list):
+    for path in path_list:
         try:
-            os.remove(temp_audio_output)
+            os.remove(path)
         except (...):
-            print(f"Odd, {temp_audio_output} couldn't be deleted or wasn't found...")
-        try:
-            os.remove(temp_srt_output)
-        except (...):
-            print(f"Odd, {temp_srt_output} couldn't be deleted or wasn't found...")
+            print(f"Odd, {path} couldn't be deleted or wasn't found...")
 
 if (__name__ == "__main__"):
-    script = """
-    What is the scariest thing that's ever happened to you?
-
-    So, I’m sitting at home last night, scrolling through my phone like usual, when I get a random text invite to a group chat. I don’t recognize the number, but curiosity gets the best of me, so I join.
-
-    The first message I see? “Make sure there’s no blood left in the trunk.”
-
-    I froze. At first, I thought it was some stupid prank or maybe someone trolling, but then the next message comes in: “We need to dump it before sunrise. No mistakes this time.”
-
-    THIS TIME? Excuse me? My first instinct was to leave the chat, but I didn’t want to seem suspicious. Then someone else in the group says, “Where’s the drop point?”
-
-    At this point, I’m sweating. I look around my room like I’m about to get sniped through the window. Then I decide to type the most nonchalant thing I could think of to blend in: “Yeah, where are we meeting again?”
-
-    Bad idea. Because the next thing I know, someone replies with, “Wait, who the f* is this?”**
-
-    I’m panicking. My thumb is hovering over the “Leave Chat” button when another message comes through: “Did we get hacked?!” And then: “If they know, we’re screwed.”
-
-    SCREWED? WHO’S SCREWED? I’m breathing like I just ran a marathon, trying to think of an excuse when one of the names in the group starts typing. But before they can send whatever terrifying thing they’re writing, I just leave the chat.
-
-    Now I’m lying in bed, staring at the ceiling, convinced I’ve stumbled into some mob operation. I thought about calling the cops, but what do I say? “Hi, I think I just accidentally joined a murder group chat”? They’d laugh me off the phone!
-
-    Anyway, if I disappear after posting this, you know why. Check my trunk.
-    """
-
+    script_path = "scripts/protective_ex"
     temp_audio_output = "temp_audio_output.mp3"
     temp_srt_output = "temp_subs.srt"
-    source_video_path = "inputs/formatted_spiral_parkour.mp4"
-    video_output_path = "outputs/output.mp4"
-    generateViralVideo(script, temp_audio_output, temp_srt_output, source_video_path, video_output_path, randomize=True, karaoke=True, lang='en', tld='com.au', flush=False)
+    source_video_path = "inputs/parkour_recording_trimmed.mkv"
+    video_output_path = f"outputs/{script_path[:8]}.mp4"
+    generateViralVideo(script_path, temp_audio_output, temp_srt_output, source_video_path, video_output_path, randomize=True, karaoke=True, lang='en', tld='com.au', flush=True)
 
